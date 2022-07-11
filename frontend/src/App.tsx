083afacbd0960eab635042228ad93b409280b7d0
artem-berlin <art.tester2020@gmail.com>
2022-07-11 16:22:19 +0200

    first

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/LoginPage";
+import AuthProvider from "./auth/AuthProvider";
+import LandingPage from "./pages/LandingPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
+import DetailsPage from "./pages/DetailsPage";
+
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <AuthProvider>
+                    <Routes>
+                        <Route path={'/'} element={<LandingPage/>}/>
+                        <Route path={'/main'} element={<MainPage/>}/>
+                        <Route path={'/login'} element={<LoginPage/>}/>
+                        <Route path={'/register'} element={<RegisterPage/>}/>
+                        <Route path={'/checkout/:shoppingCartId'} element={<CheckoutPage/>}/>
+                        <Route path={'/confirmation'} element={<ConfirmationPage/>}/>
+                        <Route path={'/:productId'} element={<DetailsPage/>}/>
+                     {/*   <Route path={'/shoppingcart'} element={<DetailsPage/>}/>*/}
+                    </Routes>
+                </AuthProvider>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
